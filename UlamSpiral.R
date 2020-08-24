a34210030ac286c5b4246b5a406f293ef4bcc1cf
Dmitry <dmp93528@gmail.com>
2020-08-25 00:57:01 +0300

    -

--- UlamSpiral.R
+++ UlamSpiral.R
@@ -1,21 +1,16 @@
 library(ggplot2)
-
-
-x = (0:8) %/% 3
-y = (0:8) %% 3
+install.packages("matlab")
+library(matlab)
 
 
 num_to_xy <- function(n){
   L <- ceiling((sqrt(n) - 1) / 2)
-  print(L)
   d <- L * 2 - 1
   r <- (n - d*d)
   r[L > 0] = r[L > 0] %% (8*L[L > 0])
-  print(r)
   part <- r %/% (2*L)
   x <- rep(0, length(part))
   p <- 2*L
-  print(part)
   x[part == 0] = L[part == 0]
   x[part == 1] = L[part == 1] - (r[part == 1] - p[part == 1])
   x[part == 2] = -L[part == 2]
@@ -28,23 +23,29 @@ num_to_xy <- function(n){
   return(data.frame(x, y))
 }
 
-num_to_xy(c(1, 4, 9, 12, 15, 16, 17, 25, 26))
+k <- 50000
 
-switch(c(1, 3, 1, 4), 0, 1, 2, 3, 4)
+nums <- 2:k
 
-d <- 1
-d[d < 0] = 0
-d
+primes <- nums[as.logical(isprime(nums))]
+hist(primes, breaks = 50)
+
+df <- num_to_xy(primes)
 
 
+ggplot(mapping = aes(x = df$x, y = df$y)) + 
+  geom_point(col = "red", size = 0.2)
   
-sqrt(5))
 
-mapply(c, replicate(3, 1:3))
+probs = rep(1, k - 1) / log(2:k)
+p_sum = sum(probs)
+r_nums <- sample(2:k, size = k/log(k), prob = probs/p_sum)
+r_nums <- sort(r_nums)
+hist(r_nums, breaks = 50)
 
-ggplot(mapping = aes(x = x, y = y)) + 
-  geom_point()
+r_df <- num_to_xy(r_nums)
 
-list
+ggplot(mapping = aes(x = r_df$x, y = r_df$y)) + 
+  geom_point(col = "red", size = 0.2)
 
-ceiling()
+c(length(r_nums), k/log(k))
